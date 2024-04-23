ab3d39ce02e9e8e89faba0c4e6a95d098545c97b
maybe.havecookies <1059758@apps.nsd.org>
2024-04-23 12:04:53 -0700

    basic spell stub

---
+++ Assets/Scripts/Combat/Spell.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Spell : ScriptableObject
+{
+    [SerializeField]
+    private string spellName;
+    [SerializeField]
+    private string incantation;
+    
+    [SerializeField]
+    private float minSpeed;
+    [SerializeField]
+    private float minAccuracy;
+    
+    [SerializeField]
+    private float critSpeedThreshhold;
+    [SerializeField]
+    private float critAccuracyThreshold;
+    
+    [SerializeField]
+    private int damage;
+    
+    /// <summary>
+    /// The name of this spell.
+    /// </summary>
+    public string SpellName => spellName;
+    
+    /// <summary>
+    /// What the player has to type in to cast this spell.
+    /// </summary>
+    public string Incantation => incantation;
+    
+    /// <summary>
+    /// The minimum typing speed required to cast this spell.
+    /// </summary>
+    public float MinSpeed => minSpeed;
+
+    /// <summary>
+    /// The minimum typing accuracy required to cast this spell.
+    /// </summary>
+    public float MinAccuracy => minAccuracy;
+    
+    /// <summary>
+    /// The threshhold of speed the player has to reach to do a critical hit.
+    /// </summary>
+    public float CritSpeedThreshhold => critSpeedThreshhold;
+    
+    /// <summary>
+    /// The threshhold of accuracy the player has to reach to do a critical hit.
+    /// </summary>
+    public float CritAccuracyThreshold => critAccuracyThreshold;
+    
+    /// <summary>
+    /// The damage this spell does.
+    /// </summary>
+    public int Damage => damage;
+}
