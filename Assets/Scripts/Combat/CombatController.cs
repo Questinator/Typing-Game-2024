2638a8e610343f6433c9ceeb2e7ad98b1fca7864
maybe.havecookies <1059758@apps.nsd.org>
2024-05-06 10:25:38 -0700

    All methods should be working in CombatController

--- Assets/Scripts/Combat/CombatController.cs
+++ Assets/Scripts/Combat/CombatController.cs
@@ -2,15 +2,20 @@ using System;
 
 public class CombatController
 {
+    private Random rand;
+    
     private CombatEntity p1;
     private CombatEntity p2;
 
-    private int turn = 0;
+    private int turn;
 
     public CombatController(CombatEntity p1, CombatEntity p2)
     {
         this.p1 = p1;
         this.p2 = p2;
+
+        turn = 0;
+        rand = new Random();
     }
     
     /// <summary>
@@ -18,49 +23,131 @@ public class CombatController
     /// </summary>
     public Spell GetRandomSpell()
     {
-        throw new NotImplementedException();
+        CombatEntity entity = getCurrentEntity();
+        return entity.Spells[rand.Next(0, entity.Spells.Length)];
     }
 
     /// <summary>
-    /// Casts the spell with the relevant position in the attacker's <c>spells</c> array.
+    /// Gets the spell with the relevant position in the attacker's <c>spells</c> array.
     /// </summary>
-    /// <param name="spellNum">The position on the attacker's <c>spells</c> array for the cast spell.</param>
+    /// <param name="spellNum">The position on the attacker's <c>spells</c> array for the spell.</param>
     public Spell GetSpell(int spellNum)
     {
-        throw new NotImplementedException();
+        CombatEntity entity = getCurrentEntity();
+        return entity.Spells[spellNum];
+    }
+
+    /// <summary>
+    /// Gets the spell with the relevant position in <c>entity</c>'s <c>spells</c> array.
+    /// </summary>
+    /// <param name="spellNum">The position in <c>entity</c>'s <c>spells</c> array for the spell.</param>
+    /// <param name="entity">The entity to get the spell from.</param>
+    /// <returns></returns>
+    public Spell GetSpell(int spellNum, CombatEntity entity)
+    {
+        return entity.Spells[spellNum];
     }
     
     /// <summary>
     /// Casts a spell.
     /// </summary>
     /// <param name="spell">Which spell to cast.</param>
-    /// <returns>The Combat's Result</returns>
+    /// <param name="accuracy">The accuracy of the typing.</param>
+    /// <param name="speed">The speed of the typing.</param>
+    /// <returns>The Combat's Result.</returns>
     public CombatResult CastSpell(Spell spell, int accuracy, int speed)
     {
-        throw new NotImplementedException();
+        switchTurn();
+        int damage = spell.Damage;
+        if (accuracy >= spell.MinAccuracy && speed >= spell.MinSpeed)
+        {
+            if (accuracy >= spell.CritAccuracyThreshold && speed >= spell.CritSpeedThreshhold)
+            {
+                return new CombatResult(spell, (int)(damage * spell.CritMultiplier));
+            }
+
+            return new CombatResult(spell, damage);
+        }
+
+        return new CombatResult(spell, 0);
     }
     
     /// <summary>
     /// Return the result of combat from an AI.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>The result of combat from the AI.</returns>
     public CombatResult doAITurn()
-    { 
-        throw new NotImplementedException();
+    {
+        CombatEntity entity = getCurrentEntity();
+        Spell spell = entity.Spells[rand.Next(0, entity.Spells.Length)];
+        if (entity.Level <= 3)
+        {
+            return CastSpell(spell, rand.Next(75, 85), rand.Next(10, 35));
+        } else if (entity.Level <= 6)
+        {
+            return CastSpell(spell, rand.Next(80, 90), rand.Next(20, 40));
+        } else if (entity.Level <= 9)
+        {
+            return CastSpell(spell, rand.Next(85, 90), rand.Next(30, 50));
+        } else if (entity.Level > 9)
+        {
+            return CastSpell(spell, rand.Next(95, 100), rand.Next(40, 50));
+        }
+        // It should never get here because the ifs encompass all values.
+        throw new ArgumentException("something isn't working right....");
     }
     
     
     public CombatResult doPlayerTurn(Spell spell, int accuracy, int speed)
     {
-        throw new NotImplementedException();
+        return CastSpell(spell, accuracy, speed);
     }
-
+    
+    /// <summary>
+    /// Gets the CombatEntity whose turn it is.
+    /// </summary>
+    /// <returns>The CombatEntity whose turn it is.</returns>
+    private CombatEntity getCurrentEntity()
+    {
+        if (turn == 0)
+        {
+            return p1;
+        }
+        else
+        {
+            return p2;
+        }
+    }
+    
+    /// <summary>
+    /// Switches the current turn.
+    /// </summary>
+    private void switchTurn()
+    {
+        if (turn == 0)
+        {
+            turn = 1;
+        }
+        else
+        {
+            turn = 0;
+        }
+    }
     public int p1Health => p1.Health;
     public int p2Health => p2.Health;
-
+    
+    /// <summary>
+    /// The result of combat.
+    /// </summary>
     public struct CombatResult
     {
         public Spell spell;
         public int damage;
+
+        public CombatResult(Spell spell, int damage)
+        {
+            this.spell = spell;
+            this.damage = damage;
+        }
     }
 }
--- Assets/Scripts/Combat/CombatEntity.cs
+++ Assets/Scripts/Combat/CombatEntity.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 public class CombatEntity : ScriptableObject
 {
     private int health;
-
+    private int level;
     private Spell[] spells;
     
     /// <summary>
     /// The amount of HP this character has left
     /// </summary>
     public int Health => health;
+    
+    /// <summary>
+    /// The level of this entity.
+    /// </summary>
+    public int Level => level;
     
     /// <summary>
     /// All spells this entity can use in combat.
