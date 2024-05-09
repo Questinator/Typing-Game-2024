0e9bcda8aebf4d3c42e4d83ac0db782de187dc39
zukey26 <atticus.pak@gmail.com>
2024-05-08 17:51:55 -0700

    Basic scene setup

---
+++ Assets/Scripts/Combat/Controller/CombatLoader.cs
@@ -0,0 +1,8 @@
+﻿namespace Combat.Controller
+{
+    public class CombatLoader
+    {
+        public static CombatLoader Instance => instance ??= new CombatLoader();
+        private static CombatLoader instance;
+    }
+}
---
+++ Assets/Scripts/Combat/Controller/CombatUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatUI : MonoBehaviour
+{
+    [SerializeField] private Transform playerPositition;
+    [SerializeField] private Transform enemyPosition;
+    [SerializeField] private Button attackButton;
+    [SerializeField] private Button runButton;
+    [SerializeField] private Button itemButton;
+    
+}
