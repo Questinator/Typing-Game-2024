56c7d43daebf5542b1a53211f5591978d45f056e
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-05-21 16:25:57 -0700

    Player triggers (#11)
    * Partial before merge from main
    
    * Partially finished triggers
    
    * Triggers - They are done

--- Assets/Scripts/Transiton.cs
+++ Assets/Scripts/Transiton.cs
@@ -12,8 +12,8 @@ public class Transiton : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad);
             Persistence.Instance.NextPlayerLocation = spawnLocation;
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
@@ -23,11 +23,6 @@ public class Persistence
     public static Persistence Instance => instance ??= new Persistence();
     private static Persistence instance;
 
-    public Vector3 NextPlayerLocation { get; set; }
+    public Vector3 NextPlayerLocation { get; set; } = UseSceneDefault;
     public static readonly Vector3 UseSceneDefault = new Vector3(-1000, -1000, -1000);
-    public Persistence()
-    {
-        NextPlayerLocation = UseSceneDefault;
-    }
-    
 }
